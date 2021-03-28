8409000ea98ffff78a09347f6010e1b4724b8e1c
Blaire Baker <bakerblaire@gmail.com>
2021-03-28 17:26:12 -0600

    done

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,20 @@
 export default (state = [], action) => {
-  return state;
+  let idx;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+    case 'REMOVE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+    case 'UPVOTE_QUOTE':
+      return state.map(quote => {return quote.id === action.quoteId 
+        ? {...quote, votes: quote.votes ? quote.votes + 1 : 1} 
+        : quote})
+    case 'DOWNVOTE_QUOTE':
+      return state.map(quote => {return quote.id === action.quoteId
+      ? {...quote, votes: quote.votes > 0 ? quote.votes - 1 : 0}
+      : quote})
+    default:
+      return state;
+  }
 }
